cd36d933714cda0d4951c4c5e80fe6ac59c5b0d0
ngoaho91 <ngoaho91@gmail.com>
2014-03-12 10:58:50 +0700

    Fixing compilation bug
    63 left

--- BambooCC/PathEngine/PathEngine.h
+++ BambooCC/PathEngine/PathEngine.h
@@ -1,9 +1,48 @@
+#include <string.h>
 #include "InputGeom.h"
+#include "PerfTimer.h"
 #include "Recast.h"
 #include "DetourNavMesh.h"
+#include "DetourNavMeshQuery.h"
+#include "DetourTileCache.h"
+#include "DetourCrowd.h"
 
 namespace PathEngine
 {
+	class BuildContext : public rcContext
+	{
+		TimeVal m_startTime[RC_MAX_TIMERS];
+		int m_accTime[RC_MAX_TIMERS];
+
+		static const int MAX_MESSAGES = 1000;
+		const char* m_messages[MAX_MESSAGES];
+		int m_messageCount;
+		static const int TEXT_POOL_SIZE = 8000;
+		char m_textPool[TEXT_POOL_SIZE];
+		int m_textPoolSize;
+
+	public:
+		BuildContext();
+		virtual ~BuildContext();
+
+		/// Dumps the log to stdout.
+		void dumpLog(const char* format, ...);
+		/// Returns number of log messages.
+		int getLogCount() const;
+		/// Returns log message text.
+		const char* getLogText(const int i) const;
+
+	protected:	
+		/// Virtual functions for custom implementations.
+		///@{
+		virtual void doResetLog();
+		virtual void doLog(const rcLogCategory /*category*/, const char* /*msg*/, const int /*len*/);
+		virtual void doResetTimers();
+		virtual void doStartTimer(const rcTimerLabel /*label*/);
+		virtual void doStopTimer(const rcTimerLabel /*label*/);
+		virtual int doGetAccumulatedTime(const rcTimerLabel /*label*/) const;
+		///@}
+	};
 	class NavMesh
 	{
 	private:
@@ -30,14 +69,14 @@ namespace PathEngine
 		struct LinearAllocator* m_talloc;
 		struct FastLZCompressor* m_tcomp;
 		struct MeshProcess* m_tmproc;
-		class dtTileCache* m_tileCache;
+		dtTileCache* m_tileCache;
 
 		int m_maxTiles;
 		int m_maxPolysPerTile;
 		float m_tileSize;
 	public:
-		NavMesh(){}
-		~NavMesh(){}
+		NavMesh();
+		~NavMesh();
 		// navigation mesh
 		void InitMesh();
 		void SetMesh(InputGeom* geom);
@@ -55,5 +94,5 @@ namespace PathEngine
 		void UpdateCrowd(const float dt);
 		void MoveAgent(const float* p);
 		void ForceAgent(const float* p);
-	}
+	};
 }
